5ef26db90aa25c4a78871fcde5dbd1b971632932
Gabriel Raiol <raiol.gabriel@hotmail.com>
2024-03-04 14:14:37 -0300

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 export default function App() {
   return (
     <section>
-      ADSFHGÇODIFHGDF
+      <header></header>
+      <div>
+        <div>
+          <img></img>
+          <h1>Race with your kart in the jungle, battle other drivers and earn as you play!</h1>
+          <a>Botao 1</a>
+          <a>Botao 2</a>
+        </div>
+
+        <div>
+          <img></img>
+        </div>
+      </div>
     </section>
   )
 }
