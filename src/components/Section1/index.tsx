a9d44d758513fa77b4ca893275e1199e421dd774
Gabriel Raiol <raiol.gabriel@hotmail.com>
2024-03-05 17:41:03 -0300

    finishing first section

--- src/components/Section1/index.tsx
+++ src/components/Section1/index.tsx
@@ -7,14 +7,16 @@ export default function Section1() {
     return (
         <section className={styles.sec1}>
             < Header />
-            <div>
-                <div>
+            <div className={styles.main}>
+                <div className={styles.firstContainer}>
                     <img src={Logo}></img>
                     <h2>Race with your kart in the jungle, battle other drivers and earn as you play!</h2>
-                    <a href="#">START</a>
-                    <a href="#">Mint your kart!</a>
+                    <div className={styles.buttonsContainer}>
+                        <a className={styles.fullfill} href="#">START ▸</a>
+                        <a href="#">Mint your kart!</a>
+                    </div>
                 </div>
-                <div>
+                <div className={styles.secondContainer}>
                     <img src={Kart}></img>
                 </div>
             </div>
