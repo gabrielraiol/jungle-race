826c6df8af1750fabb249ddbd3cfbd8713b06167
Gabriel Raiol <raiol.gabriel@hotmail.com>
2024-03-12 17:47:42 -0300

    adjusting alyer1 and layer2 of section2

--- src/components/Section2/index.tsx
+++ src/components/Section2/index.tsx
@@ -1,18 +1,9 @@
 import styles from "./Section2.module.scss";
 import Monkey from "src/assets/imgSec2.png";
-import Layer1 from "src/assets/sec2Layer1.png";
-import Layer2 from "src/assets/sec2Layer2.png";
 
 export default function Section2() {
     return (
         <section className={styles.sec2}>
-
-            <div className={styles.layer1}>
-                <img src={Layer1}/>
-            </div>
-            <div className={styles.layer2}>
-                <img src={Layer2}/>
-            </div>
             <div className={styles.main}>
                 <div className={styles.imgContainer}>
                     <img src={Monkey}/>
@@ -27,6 +18,9 @@ export default function Section2() {
                         <a href="#">Join our Community!</a>
                     </div>
                 </div>
+
+                <div className={styles.layer1}/>
+                <div className={styles.layer2}/>
             </div>
         </section>
     )
