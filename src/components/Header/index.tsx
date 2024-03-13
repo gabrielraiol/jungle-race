4d23df69048a3b0c91d7aabe0bbf710f6238aafa
Gabriel Raiol <raiol.gabriel@hotmail.com>
2024-03-13 14:25:17 -0300

    burger menu OK; still need to polish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import sLogo from "src/assets/logo2.svg";
 import styles from "./Header.module.scss";
+import { useState } from 'react';
 
 export default function Header() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <header className={styles.headerDesktop}>
+        <header className={styles.header}>
             <img src={sLogo}></img>
-            <div className={styles.navigation}>
+            <nav className={styles.navigation}>
                 <a href="#">NFTs</a>
                 <a href="#">Utilities</a>
                 <a href="#">Partners</a>
                 <a href="#">Pitchdeck</a>
-            </div>
+            </nav>
             <div className={styles.buttons}>
                 <div className={styles.container}>
                     <a href="#">Watch trailer!</a>
@@ -18,6 +26,19 @@ export default function Header() {
                 <div className={styles.container}>
                     <a className={styles.fullfill} href="#">Mint your kart!</a>
                 </div>
+            </div>
+            <div className={styles.headerMobile}>
+                <button className={`${styles.burger} ${isMenuOpen ? styles.active : ''}`} onClick={handleMenuToggle}>
+                    <span className={styles.bar} id="bar1"></span>
+                    <span className={styles.bar} id="bar2"></span>
+                    <span className={styles.bar} id="bar3"></span>
+                </button>
+                <nav className={`${styles.links} ${isMenuOpen ? styles.show : ''}`}>
+                    <a href="#">NFTs</a>
+                    <a href="#">Utilities</a>
+                    <a href="#">Partners</a>
+                    <a href="#">Pitchdeck</a>
+                </nav>
             </div>
         </header>
     )
