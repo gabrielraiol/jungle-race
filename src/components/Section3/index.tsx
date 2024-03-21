4c41fb5f61560f5b553c24a5f3eaabfadb1948f2
Gabriel Raiol <raiol.gabriel@hotmail.com>
2024-03-20 21:44:52 -0300

    swiper props

--- src/components/Section3/index.tsx
+++ src/components/Section3/index.tsx
@@ -1,9 +1,13 @@
 import styles from "./Section3.module.scss";
 import 'swiper/swiper-bundle.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import SwiperCore from 'swiper';
 import Slide1 from "src/assets/slide3_1.svg";
 import Slide2 from "src/assets/slide3_2.svg";
 import Slide3 from "src/assets/slide3_3.svg";
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 export default function Section3() {
 
@@ -14,14 +18,19 @@ export default function Section3() {
             </h2>
 
             <div className={styles.swiperContainer}>
-                <Swiper>
+                <Swiper
+                    autoplay={{ delay: 2500 }}
+                    slidesPerView={1}
+                    breakpoints={{
+                        1200: {
+                          slidesPerView: 3,
+                        },
+                      }}
+                >
                     <SwiperSlide><img src={Slide1} alt="Slide 1" /></SwiperSlide>
                     <SwiperSlide><img src={Slide2} alt="Slide 2" /></SwiperSlide>
                     <SwiperSlide><img src={Slide3} alt="Slide 3" /></SwiperSlide>
                 </Swiper>
-
-                <div className="swiper-button-prev"></div>
-                <div className="swiper-button-next"></div>
             </div>
     </section>
     )
