cdca4f57995904bcd8bebf73e5f1137326be2c71
Gabriel Raiol <raiol.gabriel@hotmail.com>
2024-04-02 17:42:19 -0300

    .

---
+++ src/components/Section4/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Section4.module.scss";
+
+export default function Section4() {
+    return(
+        <section className={styles.section}>
+            <div></div>
+        </section>
+    )
+}
